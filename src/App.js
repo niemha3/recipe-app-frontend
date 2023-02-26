21887d72486033abab17d45915fb8e0088f52364
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-02-26 15:47:25 +0200

    Create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import RecipeCard from "./components/RecipeCard"
 
-function App() {
+
+const App = () => {
+  const recipes = [
+    {
+      id: 1,
+      name: "Taco chicken salad",
+      mainIngredient: "chicken",
+      meal: "lunch",
+      calories: 650,
+      carbs: 50,
+      protein: 40,
+      fat: 20,
+      instructions: "",
+    },
+    {
+      id: 2,
+      name: "Tuna club sandwich",
+      mainIngredient: "Fish",
+      meal: "lunch",
+      calories: 650,
+      carbs: 50,
+      protein: 40,
+      fat: 20,
+      instructions: "",
+    },
+    {
+      id: 3,
+      name: "Pasta bolognese",
+      mainIngredient: "Minced pork meat",
+      meal: "lunch/dinner",
+      calories: 650,
+      carbs: 50,
+      protein: 40,
+      fat: 20,
+      instructions: "testing nodemon",
+    },
+    {
+      id: 4,
+      name: "Protein oats",
+      mainIngredient: "Oats",
+      meal: "breakfast",
+      calories: 333,
+      carbs: 30,
+      protein: 30,
+      fat: 13,
+
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Reseptejä</h1>
+      <ul>
+        {recipes.map(recipe =>
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        )}
+
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
