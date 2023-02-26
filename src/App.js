99b93665a9b23f8905311be6673d9b842f43d171
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-02-26 16:50:31 +0200

    Refactor App to fetch recipe data

--- src/App.js
+++ src/App.js
@@ -1,63 +1,31 @@
+import { useState } from "react"
 import RecipeCard from "./components/RecipeCard"
+import recipesService from './services/recipes'
 
 
 const App = () => {
-  const recipes = [
-    {
-      id: 1,
-      name: "Taco chicken salad",
-      mainIngredient: "chicken",
-      meal: "lunch",
-      calories: 650,
-      carbs: 50,
-      protein: 40,
-      fat: 20,
-      instructions: "",
-    },
-    {
-      id: 2,
-      name: "Tuna club sandwich",
-      mainIngredient: "Fish",
-      meal: "lunch",
-      calories: 650,
-      carbs: 50,
-      protein: 40,
-      fat: 20,
-      instructions: "",
-    },
-    {
-      id: 3,
-      name: "Pasta bolognese",
-      mainIngredient: "Minced pork meat",
-      meal: "lunch/dinner",
-      calories: 650,
-      carbs: 50,
-      protein: 40,
-      fat: 20,
-      instructions: "testing nodemon",
-    },
-    {
-      id: 4,
-      name: "Protein oats",
-      mainIngredient: "Oats",
-      meal: "breakfast",
-      calories: 333,
-      carbs: 30,
-      protein: 30,
-      fat: 13,
 
-    }
-  ];
+  const [recipes, setRecipes] = useState(null)
+
+  const getRecipes = async () => {
+    const response = await recipesService.getAll()
+    setRecipes(response)
+
+  }
+
 
   return (
     <div>
       <h1> Reseptejä</h1>
-      <ul>
-        {recipes.map(recipe =>
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        )}
+      <button onClick={getRecipes}>Get recipes</button>
+      {recipes &&
+        <ul>
+          {recipes.map(recipe =>
+            <RecipeCard key={recipe.id} recipe={recipe} />
+          )}
 
-      </ul>
+        </ul>
+      }
     </div>
   )
 }
