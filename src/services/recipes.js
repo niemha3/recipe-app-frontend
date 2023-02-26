73c38eae3089d702494811ee0d72124c6fe53c21
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-02-26 18:04:43 +0200

    Update baseUrl

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/recipes'
+const baseUrl = '/api/recipes'
 
 const getAll = async () => {
     const request = axios.get(baseUrl)
