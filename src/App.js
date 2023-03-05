1842c2b0e262335260bbd2d76749db25c155b26d
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-05 13:02:48 +0200

    Add BrowserRouter and Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from './components/Navbar'
-import RecipeCard from "./components/RecipeCard"
-import recipesService from './services/recipes'
+import Home from './views/Home'
+import Breakfast from './views/Breakfast'
+import Lunch from './views/Lunch'
+import Dinner from './views/Dinner'
+import Snack from './views/Snack'
+import Dessert from './views/Dessert'
+import Login from './views/Login'
 
 
 const App = () => {
 
-  const [recipes, setRecipes] = useState(null)
-
-  const getRecipes = async () => {
-    const response = await recipesService.getRecipes()
-    setRecipes(response)
-
-  }
-
-
   return (
     <div>
       <Navbar />
-      <RecipeCard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/breakfast" element={<Breakfast />} />
+          <Route path="/lunch" element={<Lunch />} />
+          <Route path="/dinner" element={<Dinner />} />
+          <Route path="/snack" element={<Snack />} />
+          <Route path="/dessert" element={<Dessert />} />
+          <Route path="/login" element={<Login />} /> */}
+        </Routes>
+
+      </BrowserRouter>
 
     </div>
   )
