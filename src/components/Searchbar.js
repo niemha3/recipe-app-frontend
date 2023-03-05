24a2ece9500ffca00e2d4d90a020acba7dcddc94
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-05 13:30:35 +0200

    Refactor Searchbar component

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,8 +3,8 @@ import SearchIcon from '@mui/icons-material/Search'
 
 const Searchbar = () => (
     <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', mt: 2 }}>
-        <SearchIcon sx={{ alignSelf: 'center' }} />
-        <TextField label="search" id="searchbar" sx={{ ml: 1, width: '25em' }} />
+        <SearchIcon si sx={{ alignSelf: 'center' }} />
+        <TextField label="search recipes..." id="searchbar" sx={{ ml: 1, width: '25em' }} />
     </Box>
 )
 
