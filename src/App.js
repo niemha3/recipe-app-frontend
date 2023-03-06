417a5646f19eebaa204be3c50d36492a3504962f
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-06 13:53:53 +0200

    Remove browser router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,79 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState } from "react"
 import Navbar from './components/Navbar'
-import Searchbar from "./components/Searchbar"
-import Home from './views/Home'
-// import Breakfast from './views/Breakfast'
-// import Lunch from './views/Lunch'
-// import Dinner from './views/Dinner'
-// import Snack from './views/Snack'
-// import Dessert from './views/Dessert'
-// import Login from './views/Login'
+import RecipeCard from "./components/RecipeCard"
+import recipesService from './services/recipes'
 
 
 const App = () => {
 
+  const [recipes, setRecipes] = useState(null)
+
+  const getRecipes = async () => {
+    const response = await recipesService.getRecipes()
+    setRecipes(response)
+
+  }
+
+
   return (
     <div>
       <Navbar />
-      <Searchbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/breakfast" element={<Breakfast />} />
-          <Route path="/lunch" element={<Lunch />} />
-          <Route path="/dinner" element={<Dinner />} />
-          <Route path="/snack" element={<Snack />} />
-          <Route path="/dessert" element={<Dessert />} />
-          <Route path="/login" element={<Login />} /> */}
-        </Routes>
+      {/* <RecipeCard /> */}
+      <button onClick={getRecipes}> Hae reseptit</button>
+      {recipes &&
+        <ul>
 
-      </BrowserRouter>
+
+          {recipes.map((recipe) => (
+            <li key={recipe.id}> {recipe.name}</li>
+          ))}
+        </ul>
+      }
+
 
     </div>
   )
 }
 
 export default App;
+
+
+
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import Navbar from './components/Navbar'
+// import Searchbar from "./components/Searchbar"
+// import Home from './views/Home'
+// // import Breakfast from './views/Breakfast'
+// // import Lunch from './views/Lunch'
+// // import Dinner from './views/Dinner'
+// // import Snack from './views/Snack'
+// // import Dessert from './views/Dessert'
+// // import Login from './views/Login'
+
+
+// const App = () => {
+
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <Searchbar />
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           {/* <Route path="/breakfast" element={<Breakfast />} />
+//           <Route path="/lunch" element={<Lunch />} />
+//           <Route path="/dinner" element={<Dinner />} />
+//           <Route path="/snack" element={<Snack />} />
+//           <Route path="/dessert" element={<Dessert />} />
+//           <Route path="/login" element={<Login />} /> */}
+//         </Routes>
+
+//       </BrowserRouter>
+
+//     </div>
+//   )
+// }
+
+// export default App;
