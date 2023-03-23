052531a8af2ae82910e41ef5045b2a5d2dfba6ee
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-23 12:08:40 +0200

    Clean up RecipeInstruction

--- src/components/RecipeInstruction.js
+++ src/components/RecipeInstruction.js
@@ -2,19 +2,16 @@ import { Paper, Grid, Typography, Divider } from '@mui/material'
 
 const RecipeInstruction = ( {instructions} ) => (
 
-    // <Paper sx={{ m:1, p:2, backgroundColor: '#FAF8FF' }}>
-        <Grid container sx={{display:'flex', flexDirection:'column', my:4, width:'90%'}}>
+        <Grid container sx={{display:'flex', flexDirection:'column', width:'100%'}}>
             <Typography variant="h5" sx={{fontWeight:'bold', mb:1}}>Instructions</Typography>
      
                 {instructions.map(instruction => (
-                <Grid key={instruction} item sx={{p:1}}>
-                    <Typography key={instruction}>{instruction}</Typography>
-                    <Divider />
-                </Grid>
+                    <Grid key={instruction} item sx={{p:1}}>
+                        <Typography key={instruction}>{instruction}</Typography>
+                        <Divider />
+                    </Grid>
                 ))}
         </Grid>
-    // </Paper>   
-  
 )
 
 export default RecipeInstruction
