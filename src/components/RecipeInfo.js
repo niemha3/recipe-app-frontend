d599c38ed7eca5d456449f5b5857a0c6e2df2a19
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-23 12:06:50 +0200

    Clean up RecipeInfo

--- src/components/RecipeInfo.js
+++ src/components/RecipeInfo.js
@@ -1,4 +1,4 @@
-import {Paper, Typography, Grid, Divider } from '@mui/material'
+import {Typography, Grid, Divider } from '@mui/material'
 
 const RecipeInfo = ({ recipe }) => (
 
@@ -7,8 +7,8 @@ const RecipeInfo = ({ recipe }) => (
 
             <Grid item xs={12}>
             <Typography variant="h4">{recipe.name}</Typography>
-
             </Grid>
+            
             <Grid item xs={9} sx={{mt:3}}>
                 <Typography sx={{fontWeight:'bold'}}> Cooking time: </Typography>
                 <Typography>{recipe.cookingTimeInMinutes} min</Typography>
@@ -19,23 +19,25 @@ const RecipeInfo = ({ recipe }) => (
                 <Typography>{recipe.calories} kcal</Typography>
             </Grid>
         </Grid>
+
             <Divider />
+
             <Grid container sx={{display:'flex', flexDirection:'row', justifyContent:'center',  alignItems:{ md:'end' }, p:2 }}>
-            <Grid item xs={5} sx={{mt:2}}>
-                <Typography sx={{fontWeight:'bold'}}> Protein: </Typography>
-                <Typography>{recipe.protein}g</Typography>
-            </Grid>
+                <Grid item xs={5} sx={{mt:2}}>
+                    <Typography sx={{fontWeight:'bold'}}> Protein: </Typography>
+                    <Typography>{recipe.protein}g</Typography>
+                </Grid>
 
-            <Grid item xs={5} sx={{mt:2}}>
-                <Typography sx={{fontWeight:'bold'}}> Carbs: </Typography>
-                <Typography>{recipe.carbohydrates}g</Typography>
-            </Grid>
+                <Grid item xs={5} sx={{mt:2}}>
+                    <Typography sx={{fontWeight:'bold'}}> Carbs: </Typography>
+                    <Typography>{recipe.carbohydrates}g</Typography>
+                </Grid>
 
-            <Grid item xs={2} sx={{mt:2}}>
-                <Typography sx={{fontWeight:'bold'}}> Fat: </Typography>
-                <Typography>{recipe.fat}g</Typography>
+                <Grid item xs={2} sx={{mt:2}}>
+                    <Typography sx={{fontWeight:'bold'}}> Fat: </Typography>
+                    <Typography>{recipe.fat}g</Typography>
+                </Grid>
             </Grid>
-        </Grid>
         </>
         
       
