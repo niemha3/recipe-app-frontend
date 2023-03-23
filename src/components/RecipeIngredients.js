92ff7a83ed9cf39cffb314518365e417dd4a426e
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-23 12:08:09 +0200

    Clean up RecipeIngredients

--- src/components/RecipeIngredients.js
+++ src/components/RecipeIngredients.js
@@ -1,20 +1,16 @@
-import { Paper, Grid, Typography, Divider } from '@mui/material'
+import { Grid, Typography, Divider } from '@mui/material'
 
-const RecipeIngredients = ( {ingredients} ) => (
+const RecipeIngredients = ({ ingredients }) => (
 
-    <Paper sx={{my:4, p:2, backgroundColor: '#FAF8FF'}}> 
-        <Grid container sx={{display:'flex', flexDirection:'column', my:4, width:'100%'}}>
+        <Grid container sx={{display:'flex', flexDirection:'column', width:'100%'}}>
             <Typography variant="h5" sx={{fontWeight:'bold', mb:1}}>Ingredients</Typography>
             {ingredients.map(ingredient => (
                 <Grid key={ingredient} item sx={{p:1}}>
                     <Typography>{ingredient}</Typography>
                     <Divider />
-                    </Grid>
+                </Grid>
             ))}
         </Grid>
-        </Paper>
-        
-  
 )
 
 export default RecipeIngredients
