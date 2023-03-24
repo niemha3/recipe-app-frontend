55f0389d77c5c28e3ccb5fd5a30c396cb0452a85
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-24 11:08:30 +0200

    Add textfield components

--- src/views/CreateRecipe.js
+++ src/views/CreateRecipe.js
@@ -1,4 +1,4 @@
-import {Box, Paper, Typography, TextField} from '@mui/material'
+import {Box, Paper, Typography, TextField, Button} from '@mui/material'
 import { useState } from 'react'
 
 const CreateRecipe = () => {
@@ -63,28 +63,36 @@ const CreateRecipe = () => {
   
 
 return (
-    <Paper sx={{mt:5}}>
-        <Typography variant ="h4"> Create new recipe</Typography>
+    <Paper sx={{backgroundColor: '#FAF8FF', display:'flex', flexDirection:'column', justifyContent: {xs:'center'}}}>
+        
 
-        <Box sx={{ display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', width:'50vw', mt:5}}> 
+        <Box sx={{ alignSelf:'center', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', borderRadius:'10px', width:'40vw', mt:15, p:3, backgroundColor:'white'}}> 
+
+        <Typography variant ="h4" sx={{fontSize: '2em', fontWeight:'bold', m:2, alignSelf: {xs: 'center', sm:'start'}}}> New recipe</Typography>
+        <Typography variant="p" sx={{m:2, fontSize:'1.1em'}}>Give required recipe info to submit a new recipe</Typography>
+
+            <Box sx={{ display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', mt:4, width:{xs:'40vw', sm:'25vw', md:'20vw', xl:'10vw'}}}> 
           
-            <TextField id="outlined-basic" label="Name" variant="outlined" size="small" margin="normal" onChange={handleNameOnChange} />
+            <TextField id="outlined-basic" label="Name" fullWidth required variant="outlined" size="small" onChange={handleNameOnChange} />
 
-            <TextField variant="outlined" label="Ingredients" multiline size="small" margin="normal"  />
+            <TextField variant="outlined" label="Ingredients" fullWidth required multiline size="small" margin="normal" onChange={handleIngredientsOnChange}  />
 
-            <TextField variant="outlined" label="Instructions" multiline size="small" margin="normal" />
+            <TextField variant="outlined" label="Instructions" fullWidth required multiline size="small" margin="normal"onChange={handleInstructionsOnChange} />
             
-            <TextField variant="outlined" label="Calories" size="small" margin="normal" />
+            <TextField variant="outlined" label="Calories" fullWidth size="small" margin="normal" onChange={handleCaloriesOnChange} />
 
-            <TextField variant="outlined" label="Main ingredient" size="small" margin="normal"  />
+            <TextField variant="outlined" label="Main ingredient" fullWidth size="small" margin="normal" onChange={handleMainIngredientOnChange}  />
          
-            <TextField variant="outlined" label="Cooking time in minutes" size="small" margin="normal" />
+            <TextField variant="outlined" label="Cooking time in minutes" fullWidth size="small" margin="normal" onChange={handleCookingTimeOnChange} />
 
-            <TextField variant="outlined" label="Protein" size="small" margin="normal"  />
+            <TextField variant="outlined" label="Protein" size="small" fullWidth margin="normal" onChange={handleProteinOnChange}  />
 
-            <TextField variant="outlined" label="Carbohydrates" size="small" margin="normal"  />
+            <TextField variant="outlined" label="Carbohydrates" fullWidth size="small" margin="normal" onChange={handleCarbohydratesOnChange}  />
 
-            <TextField variant="outlined" label="Fat" size="small" margin="normal"  />
+            <TextField variant="outlined" label="Fat" fullWidth size="small" margin="normal" onChange={handleFatOnChange}  />
+            </Box>
+
+            <Button sx={{mt:2}} disabled={!name || !ingredients || !instructions} variant="outlined">Submit</Button>
         </Box>
 
   
