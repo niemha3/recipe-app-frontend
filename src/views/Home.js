4462d10c272c554da70a744864e37edccd90f723
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-24 15:33:46 +0200

    Refactor home view

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,12 +4,14 @@ import { Paper, Typography, Grid } from '@mui/material'
 import RecipeCard from '../components/RecipeCard'
 import recipesService from '../services/recipes'
 import Searchbar from '../components/Searchbar'
+import RandomRecipes from '../components/RandomRecipes'
 
 
 
 const Home = () => {
 
     const [recipes, setRecipes] = useState(null)
+    const [randomRecipes, setRandomRecipes] = useState(null)
 
     useEffect(() => {
         const doActions = async () => {
@@ -17,17 +19,24 @@ const Home = () => {
 
             setRecipes(response)
 
+            const shuffledRecipes = response.sort(() => 0.5 - Math.random())
+
+            setRandomRecipes(shuffledRecipes.slice(0,5))
+
         }
         doActions()
 
     }, [])
 
-    console.log(recipes)
+    
 
     return (
         <> 
-            <Paper sx={{ my: 3, py: 3, backgroundColor: '#FAF8FF' }}>
-                <Typography variant="h5" sx={{fontWeight:'bold', ml: {xs: 0, md:3}, textAlign: { xs:'center', md: 'start'} }}>
+       
+          
+            <RandomRecipes randomRecipes={randomRecipes}  />
+            
+                {/* <Typography variant="h5" sx={{fontWeight:'bold', ml: {xs: 0, md:3}, textAlign: { xs:'center', md: 'start'} }}>
                     Have you tried out these?
                 </Typography>
 
@@ -39,8 +48,8 @@ const Home = () => {
                             </Grid>
                         ))}
                     </Grid>
-                }
-            </Paper>
+                } */}
+         
         </>
     )
 }
