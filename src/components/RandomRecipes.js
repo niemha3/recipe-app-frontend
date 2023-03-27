63b3203a5bda1ea4fbfc26346150e6fa919f9a51
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-27 13:01:17 +0300

    Add component to show five random recipes

--- src/components/RandomRecipes.js
+++ src/components/RandomRecipes.js
@@ -4,34 +4,38 @@ import RecipeCard from '../components/RecipeCard'
 const RandomRecipes = ({ randomRecipes }) => {
 
     return (
-        <Paper sx={{ my: 3, px: { xs: 3, md:'10%', lg: '33%'}, backgroundColor: '#FAF8FF' }}>
+        <Paper sx={{ my: 3, px: { xs: 3, md:20, xl:35}, backgroundColor: '#FAF8FF' }}>
         <Typography variant="h5" sx={{fontWeight:'bold', ml: {xs:3, md:3}}}> Have you tried these?</Typography>
 
         {randomRecipes &&
-       
-        <Grid container sx={{display:'flex', flexDirection:{xs:'row'}}}>
-            <Grid item xs={12} sm={12}> 
+       <> 
+        <Grid container sx={{display:'flex', flexDirection:{xs:'column', sm:'row'}, alignItems:'center'}}>
+            <Grid item xs={12} sm={12} lg={6} > 
                 <RecipeCard recipe={randomRecipes[0]} />
             </Grid>
-            
        
-                <Grid item xs={12} sm={6}>
+
+
+        <Grid item lg container sx={{display:'flex', flexDirection: {xs: 'column', sm:'row'}}}> 
+                <Grid item xs={12} sm={6}  lg={6}>
                     <RecipeCard recipe={randomRecipes[1]} />
                 </Grid>
 
-                <Grid item xs={12} sm={6}>
+                <Grid item xs={12} sm={6} lg={6}>
                     <RecipeCard recipe={randomRecipes[2]} />
                 </Grid>
 
-                <Grid item xs={12} sm={6}>
+                <Grid item xs={12} sm={6} lg={6}>
                     <RecipeCard recipe={randomRecipes[3]} />
                 </Grid>
 
-                <Grid item xs={12} sm={6}>
+                <Grid item xs={12} sm={6} lg={6}>
                     <RecipeCard recipe={randomRecipes[4]} />
                 </Grid>
   
         </Grid>
+        </Grid>    
+        </>
             }
             </Paper>
       
