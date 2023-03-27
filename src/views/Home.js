da3f17157b92c5bfb0a3e54f74b0672708d6aace
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-27 13:03:24 +0300

    Add state for lunchRecipes and render new components

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,6 +5,7 @@ import RecipeCard from '../components/RecipeCard'
 import recipesService from '../services/recipes'
 import Searchbar from '../components/Searchbar'
 import RandomRecipes from '../components/RandomRecipes'
+import RecipesByMeal from '../components/RecipesByMeal'
 
 
 
@@ -12,6 +13,8 @@ const Home = () => {
 
     const [recipes, setRecipes] = useState(null)
     const [randomRecipes, setRandomRecipes] = useState(null)
+    const [lunchRecipes, setLunchRecipes] = useState(null)
+
 
     useEffect(() => {
         const doActions = async () => {
@@ -23,10 +26,18 @@ const Home = () => {
 
             setRandomRecipes(shuffledRecipes.slice(0,5))
 
+            const shuffledLunch = response.filter((lunch) => lunch.meal.toLowerCase().includes('lunch')).sort(() => 0.5 - Math.random())
+
+            setLunchRecipes(shuffledLunch.slice(0,4))
+
+
         }
         doActions()
 
     }, [])
+
+    console.log(lunchRecipes)
+  
 
     
 
@@ -35,20 +46,24 @@ const Home = () => {
        
           
             <RandomRecipes randomRecipes={randomRecipes}  />
+            <RecipesByMeal lunchRecipes={lunchRecipes} />
             
-                {/* <Typography variant="h5" sx={{fontWeight:'bold', ml: {xs: 0, md:3}, textAlign: { xs:'center', md: 'start'} }}>
-                    Have you tried out these?
+            <Paper sx={{ my: 3, px: { xs: 3, md:'20', xl:35}, backgroundColor: '#FAF8FF' }}>
+                <Typography variant="h5" sx={{fontWeight:'bold', ml: {xs: 0, md:3}, textAlign: { xs:'center', md: 'start'} }}>
+                    Popular Recipes
                 </Typography>
-
+                
                 {recipes &&
-                    < Grid container sx={{display:'flex', justifyContent:'center'}}>
+                    <Grid container sx={{display:'flex', justifyContent:'center'}}>
                         {recipes.map((recipe) => (
-                            <Grid item key={recipe.id}>
+                            <Grid item xs={12} sm={6} md={4} lg={3} key={recipe.id}>
                                 <RecipeCard recipe={recipe} />
                             </Grid>
                         ))}
                     </Grid>
-                } */}
+                }
+                
+                </Paper>
          
         </>
     )
