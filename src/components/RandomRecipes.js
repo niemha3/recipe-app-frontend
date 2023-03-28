386db589bc2d8ddec04851e9b4f60a99453282b8
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-28 13:56:55 +0300

    Update padding for RandomRecipes box

--- src/components/RandomRecipes.js
+++ src/components/RandomRecipes.js
@@ -4,7 +4,7 @@ import RecipeCard from '../components/RecipeCard'
 const RandomRecipes = ({ randomRecipes }) => {
 
     return (
-        <Box sx={{ mb: 3, px: { xs: 3, md:20, xl:35}, py:3, backgroundColor: '#FAF8FF' }}>
+        <Box sx={{ mb: 3, px: { xs: 3, md:0, xl:5}, py:3, backgroundColor: '#FAF8FF' }}>
         <Typography variant="h5" sx={{fontWeight:'bold', ml: {xs:3, md:3} }}> Have you tried these?</Typography>
 
         {randomRecipes &&
