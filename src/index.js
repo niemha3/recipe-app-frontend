74a31f797aa6ea3080ea8674820e2cf63d412632
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-29 10:56:47 +0300

    Remove Routes component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Routes from './Routes'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Routes />
     <App />
   </React.StrictMode>
 );
