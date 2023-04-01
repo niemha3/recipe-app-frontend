0843f884ddefb63a62822fe62e05470c50205c16
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-04-01 14:51:42 +0300

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import { Box, Typography, Link, Grid, IconButton, BottomNavigation } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import EmailIcon from '@mui/icons-material/Email'
+
+
+const Footer = () => (
+
+  
+
+<div style={{position:'absolute', bottom:0, width:'100%', textAlign:'center'}}> 
+
+        <Typography sx={{textAlign:'center', fontSize:'0.7em'}}>© Harri Nieminen 2023</Typography>
+        <Box container sx={{display:'flex', justifyContent:'center', flexDirection:'row'}}>
+               <IconButton href="https://www.linkedin.com/in/harri-nieminen-ab1845228/">
+                 <LinkedInIcon sx={{fontSize:'0.7em'}} target="_blank" />
+               </IconButton>
+           
+               <IconButton href="https://github.com/niemha3" target="_blank">
+                 <GitHubIcon sx={{fontSize:'0.7em'}}/>
+               </IconButton>
+          
+               <IconButton>
+                 <EmailIcon sx={{fontSize:'0.7em'}} target="_blank" />
+               </IconButton>
+     
+       
+    </Box>
+    </div>
+)
+
+export default Footer
